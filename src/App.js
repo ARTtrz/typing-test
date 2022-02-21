296bcc79fda4b1cdbee9d12c8254fe9366365ba8
Artyom Kim <artem_kim_2006@list.ru>
2022-02-21 17:41:52 +0600

    we have built the User Interface, setting the countdown and setting up key listenners

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import randomWords from 'random-words';
+
+
+const NUMB_OF_WORDS = 200;
+const SECONDS = 2;
 
 function App() {
+  const [words, setWords] = useState([]);
+  const [countDown, setCountDown] = useState(SECONDS);
+  const [currentInput, setCurrentInput] = useState("")
+
+  useEffect(() => {
+    setWords(generateWords());
+  }, [])
+
+  const generateWords = () => {
+    return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords());
+  }
+
+  const start = () => {
+    let interval = setInterval(() => {
+      setCountDown((prevCountDown) => {
+        if (prevCountDown == 0) {
+          clearInterval(interval);
+        } else {
+          return (
+
+            prevCountDown - 1
+          )
+        }
+
+      });
+    }, 1000)
+  }
+
+  const handleKeyDown = (event) => {
+    console.log(event.key)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section">
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>{countDown}</h2>
+        </div>
+      </div>
+      <div className="control is-expanded section">
+        <input type="text" className="input" onKeyDown={handleKeyDown} value={currentInput} onChange={(e) => setCurrentInput(e.target.value)} />
+      </div>
+      <div className="section">
+        <button className="button is-info is-fullwidth" onClick={start}>Start</button>
+      </div>
+      <div className="section">
+        <div className="card">
+          <div className="card-content">
+            <div className="content">
+              {words.map((word, i) => (
+                <span key={i}>
+                  <span>
+                    {word.split("").map((char, idx) => (
+
+                      <span key={idx}>{char}</span>
+
+
+                    ))}
+
+                  </span>
+                  <span> </span>
+                </span>
+                // <>
+                //   <span>
+                //     {word}
+                //   </span>
+                //   <span> </span>
+                // </>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
